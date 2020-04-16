5aa9276a6378754a404847db880d02b81b955290
JeanneRbs <62756375+JeanneRbs@users.noreply.github.com>
2020-04-16 21:46:15 +0200

    Add files via upload

---
+++ TDD/unitest_Jeanne_Ramambason.py
@@ -0,0 +1,23 @@
+import VideoGames.Fighting.py
+import unittest
+
+
+class BadInputFighting(unittest.TestCase):
+	def testnameofplayersknownnames(self):
+		self.assertRaises(VideoGames.UnknownPlayerError, VideoGames.Fighting.nameofplayers, 'Jeanne')
+
+   	def testequipweapon(self):
+   		self.assertRaises(VideoGames.UnknownWeaponError, VideoGames.Fighting.equipweapon, 'Timer')
+
+   	def testtimefightoolong(self):
+   		"""in minute"""
+   		self.assertRaises(VideoGames.OutOfRangeError, VideoGames.Fighting.timefight, 8)
+
+   	def testZero(self):
+   		self.assertRaises(VideoGames.OutOfRangeError, VideoGames.Fighting.timefight, 0)
+
+   	def testNegative(self):
+   		self.assertRaises(VideoGames.OutOfRangeError, VideoGames.Fighting.timefight, -3)
+
+   	def testNonInteger(self):
+   		self.assertRaises(VideoGames.NotIntergerError, VideoGames.Fighting.timefight, 0.5)

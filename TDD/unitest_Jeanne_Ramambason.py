21f0a14927a7ac656e3645eb1819f4537f01faba
JeanneRbs <62756375+JeanneRbs@users.noreply.github.com>
2020-04-17 21:32:47 +0200

    Update unitest_Jeanne_Ramambason.py

--- TDD/unitest_Jeanne_Ramambason.py
+++ TDD/unitest_Jeanne_Ramambason.py
@@ -1,34 +1,43 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-#from videogames import *
-import videogames
+import VideoGames
 import unittest
-from videogames import *
+from VideoGames import *
 
-SmashBros = Fighting("fantasy",2,13)import VideoGames
-
+SmashBros = Fighting("fantasy",2,13)
 
 
 class BadInputFighting(unittest.TestCase):
-	def testnameofplayersknownnames(self):
-		self.assertRaises(VideoGames.UnknownPlayerError, VideoGames.Fighting.nameofplayers, 'Jeanne')
+   def testnameofplayer1knownnames(self):
+      """nameofplayer1 should dail with name not in the list"""
+      with self.assertRaises(UnknownPlayerError):
+         SmashBros.nameofplayer1('Jeanne')
 
-   	def testequipweapon(self):
-   		self.assertRaises(VideoGames.UnknownWeaponError, VideoGames.Fighting.equipweapon, 'Timer')
+   def testequipweapon(self):
+      """equiweapon should fail with name of weapons not in the list"""
+      with self.assertRaises(UnknownWeaponError):
+         SmashBros.equipweapon('Timer')
 
-   	def testtimefightoolong(self):
-   		"""in minute"""
-   		self.assertRaises(VideoGames.OutOfRangeError, VideoGames.Fighting.timefight, 8)
+   def testtimefightoolong(self):
+   	"""timefight should fail with time out of range"""
+   	with self.assertRaises(OutOfRangeError):
+         SmashBros.timefight(9)
 
-   	def testZero(self):
-   		self.assertRaises(VideoGames.OutOfRangeError, VideoGames.Fighting.timefight, 0)
+   def testZero(self):
+      """timefight should fail with zero input"""
+      with self.assertRaises(OutOfRangeError):
+         SmashBros.timefight(0)
 
-   	def testNegative(self):
-   		self.assertRaises(VideoGames.OutOfRangeError, VideoGames.Fighting.timefight, -3)
+   def testNegative(self):
+      """timefight should fail with negative input"""
+      with self.assertRaises(OutOfRangeError):
+         SmashBros.timefight(-3)
 
-   	def testNonInteger(self):
-   		self.assertRaises(VideoGames.NotIntergerError, VideoGames.Fighting.timefight, 0.5)
+   def testNonInteger(self):
+      """timefight should fail with non-interger input"""
+      with self.assertRaises(NotIntegerError):
+         SmashBros.timefight(0.5)
 		
 if __name__ == "__main__":
    unittest.main()

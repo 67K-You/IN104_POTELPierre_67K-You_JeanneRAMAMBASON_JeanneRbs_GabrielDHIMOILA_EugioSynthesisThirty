1781f2b206b18dc0191a72b604e8bb53ddc971a1
JeanneRbs <62756375+JeanneRbs@users.noreply.github.com>
2020-05-20 18:30:35 +0200

    Create unittests_genetic.py

---
+++ GeneticAlgorithm/unittests_genetic.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import genetic
+from genetic import *
+
+class Testindividual(unittest.TestCase):
+def testmutate(self):
+		"""mutate should fail if not (0<=rate<=1)"""
+		      with self.assertRaises(OutOfRangeError):
+				Individual.mutate(self,-1)
+        
+class Testpopulation(unittest.TestCase):
+def testpopulationsizeinteger(self):
+"""Population.__init__ should fail with non-integer input"""
+with self.assertRaises(NotIntegerError):
+			Population(self,0.5,[1,2])
+      def testpopulationsizepositive(self):
+	   """Population.__init__ should fail with negative input"""
+	   with self.assertRaises(OutOfRangeError):
+			Population(self,-1,[1,2])
+      def testliste(self):
+      """Population.__init__ should fail if liste is not a type 'list'"""
+      with self.assertRaises(NotListError):
+      Population(self,3,2)
+      
+  class Testget_sub_list(unitest.TestCase):
+  def testlist(self):
+    """ get_sub_list should fail if len(liste)=0"""
+    with self.Raises(OutOfRangeError):
+    get_sub_list([])
+    
+    def testpositiveiteration(self):
+    """ get_sub_list should fail if iteration<=0"""
+    with self.Raises(OutOfRangeError):
+    get_sub_list([1,2],10,-1)
+    
+    def testintegeriteration(self):
+    """get_sub_list should fail if iteration is non-integer"""
+    with self.Raises(NotIntegerError):
+    get_sub_list([1,2],10,0.5)
+
